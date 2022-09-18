faf8ddf40443a28d10ebc4c267bf0d4a6f217252
Luke Brogee <lukebrogee@gmail.com>
2022-09-17 20:21:32 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        double number = 8;
+
+        System.out.println("Is the number even?");
+
+        if (number % 2 != 0){
+            System.out.println("The number is odd!");
+            } else {
+            System.out.println("The number is even!");
+        }
+
+    }
+}
